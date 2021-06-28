4b481fdf5405e27cb7359d383296595892a2da45
翁子皓 <x142y857@gmail.com>
2021-06-28 09:53:33 +0800

    12-1

---
+++ Game.kt
@@ -0,0 +1,91 @@
+
+import java.lang.Math.pow
+import java.lang.Math.random
+
+fun main() {
+    val name = "翁子皓"
+    var healthPoints = 89
+    var isBlessed = true
+    val isImmortal = false
+
+    val player = player()
+    val karma = (pow(random(),(110-healthPoints)/100.0)*20).toInt()
+    //Aura
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal, karma)
+
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+    val inebriationLevel = castFireball()
+    val inebriationStatus = inebriationLevelStatus(inebriationLevel)
+
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println("光環顏色：$auraColor" + "    走運嗎？${if (isBlessed) "是的" else "很背"}")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(
+    isBlessed: Boolean,
+    healthPoints: Int,
+    isImmortal: Boolean,
+    karma: Int
+): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = when (karma) {
+        in (0..5) -> "紅色"
+        in (6..10) -> "橘色"
+        in (11..15) -> "紫色"
+        in (16..20) -> "綠色"
+        else -> "無光環"
+    }
+    return auraColor
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean)=
+    when (healthPoints) {
+        100 -> "健康狀態極佳"
+        in 90..99 -> "有一些小擦傷"
+        in 75..89 -> if (isBlessed) {
+            "雖有一些傷口，但恢復很快"
+        } else {
+            "有一些傷口"
+        }
+        in 15..74 -> "嚴重受傷"
+        //顯示玩家狀態
+        else -> "情況不妙"
+    }
+
+
+
+private fun castFireball(numFireballs: Int = 2) : Int {
+
+    println("一個FireBall出現了. (x$numFireballs)")
+
+    var minInebriation : Int
+    if (numFireballs < 50) minInebriation = numFireballs else minInebriation = 50
+    val fireballInebriation = (minInebriation..50).random()
+
+    return fireballInebriation
+}
+
+private fun inebriationLevelStatus(inebriationLevel: Int) {
+
+    val inebriationStatus = when (inebriationLevel) {
+        in 41..50 -> "..爛醉如泥"
+        in 31..40 -> "大醉"
+        in 21..30 -> "醉了"
+        in 11..20 -> "微醉"
+        in 1..10 -> "微醺"
+        else -> "沒醉"
+    }
+
+    println("醉酒程度: $inebriationLevel")
+    println("醉酒狀態: $inebriationStatus")
+
+}
