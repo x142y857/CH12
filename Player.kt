07fee7c198c2d58d3a27bc62fd39222e5d1f1fb6
翁子皓 <x142y857@gmail.com>
2021-06-28 10:49:18 +0800

    12-9

--- Player.kt
+++ Player.kt
@@ -1,6 +1,9 @@
 class Player {
     val name ="翁子皓"
         get() = field.capitalize()
+        set(value) {
+            field = value.trim()
+        }
     fun castFireball(numFireballs: Int = 2)=
         println("一個FireBall出現了. (x$numFireballs)")
 }
